ec9d662cfe61f6d98ffb27883d68e8686251c209
rakibulhasan933 <rakibulmd933@gmail.com>
2023-09-04 17:03:32 +0600

    [ADD] route handle created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from 'next/image'
+import { UserIProps } from "../../types";
 
-export default function Home() {
+
+async function getUsers() {
+  const res = await fetch("http://localhost:3000/api/users", {
+    cache: "no-cache"
+  })
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getUsers();
+
   return (
     <main>
-      <h2>Hello World</h2>
+      <h2>All Users</h2>
+      <ul>
+        {
+          data?.users.map((user: UserIProps) => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
     </main>
   )
 }
